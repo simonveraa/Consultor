fc43cceec5a5940a3254ac5a5d21c624d3313bc6
gatotui <gatotui14@gmail.com>
2025-01-10 11:54:37 -0300

    lógica para no repetir exámenes con mismo código

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
 const buscarForm = document.getElementById("buscarForm")
 const resultado = document.getElementById("resultado")
+const sinResultado = document.getElementById("sin-resultado")
 const resultadoTitulo = document.getElementById("resultado-titulo")
+const tabla = document.getElementById("tabla")
 
-      
+buscarForm.addEventListener("submit", function (e) {
 
-      buscarForm.addEventListener("submit", function (e) {
-        e.preventDefault()
-        resultadoTitulo.classList.remove("d-none")
-        const examenBuscado = document
-          .getElementById("buscar")
-          .value.toLowerCase()
-        resultado.innerHTML = ""
+  const examenBuscado = document.getElementById("buscar").value.toLowerCase()
+  e.preventDefault()
+  tabla.classList.add("d-none")
+  resultadoTitulo.classList.add("d-none")
+  resultado.innerHTML = ""
+  sinResultado.innerHTML = ""
 
-        const resultadoFiltrados = datos.filter((d) =>
-          d.examen.toLowerCase().includes(examenBuscado)
-        )
+  const resultadoFiltrados = datos.filter((d) => d.examen.toLowerCase().includes(examenBuscado))
 
-        if (resultadoFiltrados.length > 0) {
-          resultadoFiltrados.forEach((r) => {
-            const row = document.createElement("tr")
-            row.innerHTML = `
-              <td><strong>${r.examen}</strong></td>
-              <td>${r.establecimiento}</td>
-              <td>${r.codigo ? r.codigo : ''}</td>
-              <td>${r.codigoBK ? r.codigoBK : ''}</td>
-              <td>${r.nota ? r.nota : ''}</td>
-            `
-            resultado.appendChild(row)
-          })
-        } else {
-          resultado.innerHTML = `
-            <tr>
-              <td colspan="3"><strong>No se encontró el examen:</strong> 
-                ${document.getElementById("buscar").value}</td>
-            </tr>
-            <tr>    
-              <td colspan="3">Busque una variable del nombre: 
-                ${document.getElementById("buscar").value}</td>
-            </tr>
+  const codigosVistos = new Set(); // <-- para registrar los códigos únicos. El Set es una estructura de datos que no permite repeticiones.
+
+  if (resultadoFiltrados.length > 0) {
+      tabla.classList.remove("d-none")
+      resultadoTitulo.classList.remove("d-none")
+      resultadoFiltrados.forEach((r) => {
+        if (!r.codigo || !codigosVistos.has(r.codigo)) { // <-- si no tiene código o en el Set ya está el código
+          const row = document.createElement("tr")
+          row.innerHTML = `
+            <td><strong>${r.examen}</strong></td>
+            <td>${r.establecimiento}</td>
+            <td>${r.codigo ? r.codigo : ''}</td>
+            <td>${r.nota ? r.nota : ''}</td>
           `
+          resultado.appendChild(row)
+
+          if (r.codigo) {
+            codigosVistos.add(r.codigo) // <-- para agregar los elementos que tengan código al Set
+            console.log(codigosVistos)
         }
+      }  
       })
+    } else {
+      sinResultado.innerHTML = `
+      <div class="fs-2 text-danger mt-4"><strong>No se encontró el examen:</strong> "${examenBuscado}"</div>
+      <div class="fs-4">Busque una variante de <i>"${examenBuscado}"</i>.</div>
+      `
+    }
+  }
+)
